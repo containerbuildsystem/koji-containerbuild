90f52abfc9ceeb38b6a5b46f132768a0068ab78d
Robert Cerven <rcerven@redhat.com>
2019-12-18 20:55:22 +0100

    Allow additional properties for containerbuild
    Signed-off-by: Robert Cerven <rcerven@redhat.com>

--- koji_containerbuild/plugins/builder_containerbuild.py
+++ koji_containerbuild/plugins/builder_containerbuild.py
@@ -643,7 +643,7 @@ class BuildContainerTask(BaseContainerTask):
                         "description": "User defined dictionary containing custom metadata",
                     },
                 },
-                "additionalProperties": False
+                "additionalProperties": True
             }
         ],
         "minItems": 3

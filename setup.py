562df357dc88a832f14bea196259f518ec1114e2
Martin Basti <mbasti@redhat.com>
2022-06-30 10:47:16 +0200

    Split requirements for py2 and py3
    py2 requires old version of jsonschema but we don't have to
    prevent new jsonchema versions t be installed for py3
    
    fixes: #242
    
    Signed-off-by: Martin Basti <mbasti@redhat.com>

--- setup.py
+++ setup.py
@@ -10,13 +10,21 @@ class TitoDist(sdist):
         subprocess.call(["tito", "build", "--tgz", "-o", "."])
 
 
-def get_requirements(requirements_file='requirements.txt'):
+def _get_requirements(requirements_file='requirements.txt'):
     with open(requirements_file) as f:
         return [
             line.split('#')[0].rstrip()
             for line in f.readlines()
             if not line.startswith('#')
             ]
+
+
+def _install_requirements():
+    if sys.version_info[0] >= 3:
+        requirements = _get_requirements('requirements.txt')
+    else:
+        requirements = _get_requirements('requirements-py2.txt')
+    return requirements
 
 
 class Py2CLIOnlyBuild(build_py):
@@ -46,7 +54,7 @@ setup(
         'koji_containerbuild',
         'koji_containerbuild.plugins',
     ],
-    install_requires=get_requirements(),
+    install_requires=_install_requirements(),
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Topic :: Internet",
